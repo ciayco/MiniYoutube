1c698e764969bdc9eebb19ef866168b6e7afbb43
Ciayco <ciayco07@gmail.com>
2015-04-14 17:57:04 +0300

    Update code project
    update code project

---
+++ Update code/guncelleme/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(38, 78)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(381, 29)
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(163, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(129, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Güncelleniyor"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.ClientSize = New System.Drawing.Size(444, 137)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Güncelleme"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Update code/guncelleme/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+
+            Dim client As New System.Net.WebClient
+            ProgressBar1.Value = 25
+
+            Dim processes As Process() = Process.GetProcessesByName("MiniYouTube")
+            For Each process As Process In processes
+                process.Kill()
+            Next
+
+            ProgressBar1.Value = 50
+
+
+            client.DownloadFile("https://www.dropbox.com/s/z2qog0sasy4yb8s/MiniYouTube.exe?dl=1", "C:\MiniYouTube\MiniYouTube.exe")
+            ProgressBar1.Value = 100
+
+            Process.Start("C:\MiniYouTube\MiniYouTube.exe")
+            Me.Close()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
